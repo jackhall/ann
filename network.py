123d2a0e48999d8c5370003440e6a3d550dee986
Jack Hall <jackwhall@protonmail.com>
2023-10-25 23:50:25 -0400

    implemented Network::execute_node

--- network.py
+++ network.py
@@ -1,223 +1,169 @@
 from dataclasses import dataclass
-from contextlib import contextmanager
-import random
 from typing import (
+    Any,
     Callable,
-    Collection,
     Hashable,
     Iterable,
-    Optional,
     Protocol,
+    Sequence,
     Type,
     TypeVar,
 )
 import itertools as it
+import functools as ft
 
 import networkx as nx
-import bitstring as bs
 
 
-@contextmanager
-def random_seed(seed: int):
-    state = random.getstate()
-    random.seed(seed)
-    try:
-        yield
-    finally:
-        random.setstate(state)
+Downflow = TypeVar("Downflow")
+Upflow = TypeVar("Upflow")
+NodeID = Hashable
 
 
-U = TypeVar("U")
-D = TypeVar("D")
+class Node(Protocol[Downflow, Upflow]):
+    id: NodeID
 
-
-class Node(Protocol[U, D, A]):
     @classmethod
-    def new(cls) -> "Node":
+    def new(cls, **kwargs) -> "Node":
         ...
 
-    def gather(self, downflows: list[D], upflows: list[U]) -> A:
+    def gather(self, downflows: Sequence[Downflow], upflows: Sequence[Upflow]) -> None:
         ...
 
-    def propagate_downstream(self, act: A, targets: Collection["Link"]) -> list[D]:
+    def split_downstream(self, targets: Sequence["DownstreamStateLink"]) -> list[Downflow]:
         ...
 
-    def propagate_upstream(self, act: A, sources: Collection["Link"]) -> list[U]:
+    def split_upstream(self, sources: Sequence["UpstreamStateLink"]) -> list[Upflow]:
         ...
 
-    def update(self):
+    def adapt(self) -> None:
         ...
 
 
-class Link(Protocol[U, D]):
+class SimpleLink(Protocol[Downflow, Upflow]):
     @classmethod
-    def new(cls) -> "Link":
+    def new(cls) -> "SimpleLink":
         ...
 
-    def pull_downstream(self, node: Node) -> D:
+    def pull_downstream(self, node: Node) -> Downflow:
         ...
 
-    def pull_upstream(self, node: Node) -> U:
-        ...
-
-
-class UpstreamStateLink(Link[U, D], Protocol[U, D]):
-    def send_upstream(self, upflow: U) -> None:
+    def pull_upstream(self, node: Node) -> Upflow:
         ...
 
 
-class DownstreamStateLink(Link[U, D], Protocol[U, D]):
-    def send_downstream(self, downflow: D) -> None:
+class UpstreamStateLink(
+    SimpleLink[Downflow, Upflow], Protocol[Downflow, Upflow]
+):
+    def send_upstream(self, upflow: Upflow) -> None:
         ...
 
 
-class StatefulLink(UpstreamStateLink[U, D], DownstreamStateLink[U, D], Protocol[U, D]):
+class DownstreamStateLink(SimpleLink[Downflow, Upflow], Protocol[Downflow, Upflow]):
+    def send_downstream(self, downflow: Downflow) -> None:
+        ...
+
+
+class StatefulLink(
+    UpstreamStateLink[Downflow, Upflow], 
+    DownstreamStateLink[Downflow, Upflow], 
+    Protocol[Downflow, Upflow]
+):
     ...
 
 
 @dataclass
 class Network:
     node_cls: Type[Node]  # need to know: id scheme
-    link_cls: Type[Link]  # need to know: whether links are stateful
+    link_cls: Type[SimpleLink]  # need to know: whether links are stateful
     graph: nx.DiGraph
-    scheduler: Callable[[nx.DiGraph], Iterable[Hashable]]
+    scheduler: Callable[[nx.DiGraph], Iterable[NodeID]]
 
-    def l(self, s_id: Hashable, t_id: Hashable) -> Link:
+    @ft.cached_property
+    def propagate(self) -> Callable[[NodeID], None]:
+        Neighbors = Sequence[tuple[StatefulLink, Node]]
+
+        def observe(n_id: NodeID) -> tuple[Neighbors, Neighbors]:
+            source_pairs, target_pairs = self.links_in(n_id), self.links_out(n_id)
+            self.n(n_id).gather(
+                [l.pull_downstream(s) for l, s in source_pairs], 
+                [l.pull_upstream(t) for l, t in target_pairs],
+            )
+            return source_pairs, target_pairs  # type: ignore
+
+        if hasattr(self.link_cls, "send_downstream"):
+            def stimulate_targets(n_id: NodeID) -> tuple[Neighbors, Neighbors]:
+                source_pairs, target_pairs = observe(n_id)
+                target_links = [p[0] for p in target_pairs]
+                downflows = self.n(n_id).split_downstream(target_links)
+                for link, downflow in zip(target_links, downflows):
+                    link.send_downstream(downflow)
+                return source_pairs, target_pairs
+        else:
+            stimulate_targets = observe
+
+        if hasattr(self.link_cls, "send_upstream"):
+            def stimulate_sources(n_id: NodeID) -> None:
+                source_pairs, _ = stimulate_targets(n_id)
+                source_links = [p[0] for p in source_pairs]
+                upflows = self.n(n_id).split_upstream(source_links)
+                for link, upflow in zip(source_links, upflows):
+                    link.send_upstream(upflow)
+        else:
+            def stimulate_sources(n_id: NodeID) -> None:
+                stimulate_targets(n_id)
+        
+        return stimulate_sources
+
+    def l(self, s_id: NodeID, t_id: NodeID) -> SimpleLink:
         return self.graph[s_id][t_id]["obj"]
 
-    def n(self, n_id: Hashable) -> Node:
+    def n(self, n_id: NodeID) -> Node:
         return self.graph.nodes[n_id]["obj"]
 
-    def links_in(self, n_id: Hashable) -> list[tuple[Link, Node]]:
+    def links_in(self, n_id: NodeID) -> list[tuple[SimpleLink, Node]]:
         return [
             (self.l(s_id, n_id), self.n(s_id))
             for s_id in self.graph.predecessors(n_id)
         ]
 
-    def links_out(self, n_id: Hashable) -> list[tuple[Link, Node]]:
+    def links_out(self, n_id: NodeID) -> list[tuple[SimpleLink, Node]]:
         return [
             (self.l(n_id, t_id), self.n(t_id))
             for t_id in self.graph.successors(n_id)
         ]
 
-    def simulate_flow(self):
+    def simulate_flow(self) -> None:
         # how can the scheduler tell when to halt?
         for n_id in self.scheduler(self.graph):
-            source_pairs = self.links_in(n_id)
-            target_pairs = self.links_out(n_id)
+            self.propagate(n_id)
 
-            node = self.n(n_id)
-            activation = node.gather(
-                [l.pull_downstream(s) for l, s in source_pairs], 
-                [l.pull_upstream(t) for l, t in target_pairs],
-            )
-
-            # get these conditionals out of the loop
-            if hasattr(self.link_cls, "send_downstream"):
-                source_links = [pair[0] for pair in source_pairs]
-                node.propagate_downstream(activation, source_links)
-
-            if hasattr(self.link_cls, "send_upstream"):
-                target_links = [pair[0] for pair in source_pairs]
-                node.propagate_upstream(activation, target_links)
-
-    def update_nodes(self):
+    def adapt_nodes(self) -> None:
         for _, node_attrs in self.graph.nodes.items():
-            node_attrs["obj"].update()
+            node_attrs["obj"].adapt()
 
     def simulation(self):
         self.simulate_flow()
         yield self
 
         while True:
-            self.update_nodes()
+            self.adapt_nodes()
             self.simulate_flow()
             yield self
 
-    def connect_all(self, sources: Iterable[Node], targets: Iterable[Node], **kwargs) -> None:
+    def connect_all(
+        self,
+        sources: Iterable[NodeID],
+        targets: Iterable[NodeID],
+        **kwargs
+    ) -> None:
         self.graph.add_edges_from([
             (s, t, self.link_cls.new(**kwargs)) 
             for s, t in it.product(sources, targets)
         ])
 
-
-@dataclass
-class NeuralNetwork:
-    graph: nx.DiGraph
-    layers: dict[str, int]
-
-    @staticmethod
-    def node_ids(layer, n) -> list[str]:
-        return [f"{layer}-{i}" for i in range(n)]
-
-    @classmethod
-    def empty(cls, num_inputs, num_outputs) -> "NeuralNetwork":
-        network = nx.DiGraph()
-        network.add_nodes_from(
-            cls.node_ids("in", num_inputs) 
-            + cls.node_ids("out", num_outputs)
-        )
-        return cls(network, {"in": num_inputs, "out": num_outputs})
-
-    @classmethod
-    def perceptron(
-        cls,
-        layer_counts: list[int],
-        weight_factory: Callable[[], float]
-    ) -> "NeuralNetwork":
-        assert len(layer_counts) > 2, "no hidden layers"
-        self = cls.empty(layer_counts[0], layer_counts[-1])
-
-        source_ids = cls.node_ids("in", layer_counts[0])
-
-        for layer, n in enumerate(layer_counts[1:-1], start=1):
-            node_ids = cls.node_ids(layer, n)
-            node_objs = [Neuron.random() for _ in range(n)]
-            self.graph.add_nodes_from(zip(node_ids, node_objs))
-            self.connect_all(source_ids, node_ids, weight_factory=weight_factory)
-
-            source_ids = node_ids
-            self.layers[str(layer)] = n
-        
-        return self
-
-    def connect_all(self, sources: Iterable[Node], targets: Iterable[Node], **kwargs) -> None:
-        link_ids = it.product(sources, targets)
-        link_tuples = [
-            (s, t, Link.random(s, t, **kwargs)) for s, t in link_ids
-        ]
-        self.graph.add_edges_from(link_tuples)
-
-
-@dataclass
-class Synapse:
-    weight: float
-    paid: float
-
-    @classmethod
-    def new(
-        cls,
-        weight_factory: Optional[Callable[[], float]] = None,
-    ) -> "Synapse":
-        if weight_factory is None:
-            weight_factory = lambda: random.uniform(-1, 1)
-        return cls(weight_factory(), 0.0)
-
-
-@dataclass
-class Neuron:
-    bias: float
-    sensed: bs.Bits
-    dreamed: bs.Bits
-    account: float
-
-    @classmethod
-    def new(cls, bias_factory: Optional[Callable[[], float]] = None) -> "Neuron":
-        if bias_factory is None:
-            bias_factory = lambda: random.uniform(-1, 1)
-        return cls(bias_factory(), bs.Bits(), bs.Bits(), 0.0)
-
-
-def build_xor() -> NeuralNetwork:
-    return NeuralNetwork.perceptron([2, 2, 1, 1], lambda: random.uniform(-1, 1))
+    def add_nodes(self, kwargs_list: Iterable[dict[str, Any]]) -> None:
+        for kwargs in kwargs_list:
+            node = self.node_cls.new(**kwargs)
+            self.graph.add_node(node.id, obj=node)
 
